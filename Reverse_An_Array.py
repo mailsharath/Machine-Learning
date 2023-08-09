052b0a61862b1f79b350403d00c8c0e2feca8e9a
sharath <mailsharath@hotmail.com>
2023-08-09 12:31:20 -0400

    1st commit

---
+++ Reverse_An_Array.py
@@ -0,0 +1,13 @@
+def reverse_array(nums):
+    """
+    Args:
+     nums(list_int32)
+    Returns:
+     list_int32
+    """
+    return nums[::-1]
+
+
+output_string = reverse_array(input('Enter a string or list:'))
+
+print(output_string)
