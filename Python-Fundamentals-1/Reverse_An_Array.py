f4a61b99562f54f2b21b9daee1e07f4361dafd4a
sharath <mailsharath@hotmail.com>
2023-08-10 21:19:08 -0400

    New Function

--- Python-Fundamentals-1/Reverse_An_Array.py
+++ Python-Fundamentals-1/Reverse_An_Array.py
@@ -5,7 +5,15 @@ def reverse_array(nums):
     Returns:
      list_int32
     """
-    return nums[::-1]
+    l, r = 0, len(nums)-1
+    while l < r:
+        nums[l], nums[r] = nums[r], nums[l]
+        l += 1
+        r -= 1
+    return nums
 
 
-print(reverse_array(input('Enter a string or list:')))
+nums1 = [1, 2, 3, 4, 5, 6]
+
+print(reverse_array(nums1))
+
