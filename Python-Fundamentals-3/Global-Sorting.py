7bcacf74aba07643bed3df56f9b44abbed7d2b4e
sharath <mailsharath@hotmail.com>
2023-08-26 10:07:26 -0400

    s

---
+++ Python-Fundamentals-3/Global-Sorting.py
@@ -0,0 +1,10 @@
+import json
+
+file1 = '/Users/sharath/Desktop/cars_data_multi_line.json'
+with open(file1, 'r') as read_json:
+    for line in read_json:
+        json_data = json.loads(line)
+        print(json_data)
+
+
+
